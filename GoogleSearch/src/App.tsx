ce826e6acbc888f09eddfc4f6f1b25e26d2a0183
d2ni3l <107326751+d2ni3l@users.noreply.github.com>
2023-03-09 23:03:58 +0100

    Tailwind CSS setup

---
+++ GoogleSearch/src/App.tsx
@@ -0,0 +1,12 @@
+import react from 'react'
+
+function App() {
+
+  return (
+  <div>
+     <h1 className='text-xl text-center text-red-700'>Hello world</h1>
+    </div>
+  )
+}
+
+export default App;
